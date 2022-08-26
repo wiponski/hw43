78ceb51c58bf5002d6b9fca152f932a5656d26db
Kubanychbek Alymbekov <alymbekovkubanychbek@gmail.com>
2022-08-26 18:51:19 +0600

    part2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpPrincipal;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.*;
@@ -8,6 +7,9 @@ import java.net.InetSocketAddress;
 import java.net.URI;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
@@ -34,7 +36,51 @@ public class Main {
     private static void initRoutes(HttpServer server){
         server.createContext("/", Main::handleRequest);
         server.createContext("/apps", Main::appRequest);
-        server.createContext("/apps/profile", Main::profileRequest);
+       // server.createContext("/apps/profile", Main::profileRequest);
+        server.createContext("/index", Main::fileRequest);
+    }
+
+    private static void fileRequest(HttpExchange exchange) {
+
+        try {
+            exchange.getResponseHeaders().add("Content-Type", "text/plain; charset=utf-8");
+
+            int response = 404;
+            int length = 0;
+            exchange.sendResponseHeaders(response, length);
+            try (PrintWriter writer = getWriterFrom(exchange)) {
+                Path path = Paths.get("homework/index.html");
+                Path fileName = path.getFileName();
+
+                var data = Files.readAllBytes(path);
+
+                String content = new String(data);
+
+
+
+
+                if (Files.exists(path)) {
+                    var e =Files.readAllBytes(path);
+                    write(writer, "1", path.toString());
+                    write(writer, "2", fileName.toString());
+                    write(writer, "4",content);
+                    write(writer, "4", String.valueOf(exchange.getResponseBody()));
+
+                    writeData(writer, exchange);
+                    writer.flush();
+                }else{
+
+
+
+                    writeData(writer, exchange);
+                    writer.flush();
+                }
+            }
+
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
     }
 
     private static void handleRequest(HttpExchange exchange){
